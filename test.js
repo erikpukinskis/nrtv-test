ff278a5a48c36455bf258c6dee0870a3b7bbcd30
Erik Pukinskis <erik@snowedin.net>
2015-07-14 13:36:07 -0700

    Really simple test runner

---
+++ test.js
@@ -0,0 +1,52 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
+define(["chai"], function(chai) {
+
+  function test(setup, description, test) {
+    if (!test) {
+      test = description
+      description = setup
+      setup = undefined
+    }
+
+    function done(extra) {
+      clearTimeout(timer)
+      console.log("  ✓ ", description, extra ? extra : "")
+    }
+
+    if (test.only && description != test.only) {
+      setup && setup(function() {})
+      return done(" [ SKIPPED ]")
+    }
+
+    var expect = chai.expect
+
+    var timer = setTimeout(
+      function() {
+        var message = "Got stuck in test \""+description+"\":\n"+runTest
+        if (setup) {
+          message += "\n... or or setup:\n"+setup
+        }
+        throw new Error(message)
+      },
+      1000
+    )
+
+    var runTest = test.bind(null, chai.expect)
+
+    var runAndDone = runTest.bind(null, done)
+
+    try {
+      if (setup) {
+        setup(runAndDone)
+      } else {
+        runAndDone()
+      }
+    } catch (e) {
+      console.log(" ⚡⚡⚡ "+description + " ⚡⚡⚡")
+      throw(e)
+    }
+  }
+
+  return test
+})
