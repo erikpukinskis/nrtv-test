1a1ba68129ef0acab3109b33de0c1e52509ea819
Erik Pukinskis <erik@snowedin.net>
2015-07-21 01:44:19 -0700

    Dump treelog dep and use commonjs

--- test.js
+++ test.js
@@ -1,59 +1,54 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
+var chai = require("chai")
 
-define(["chai"], function(chai) {
+var only
 
-  var only
+function test(setup, description, test) {
+  if (!test) {
+    test = description
+    description = setup
+    setup = undefined
+  }
 
-  function test(setup, description, test) {
-    if (!test) {
-      test = description
-      description = setup
-      setup = undefined
-    }
+  if (only && description != only) {
+    setup && setup(function() {})
 
-    if (only && description != only) {
-      setup && setup(function() {})
+    console.log(" (?) "+description+" (skipped)")
 
-      console.log(" (?) "+description+" (skipped)")
+    return
+  }
 
-      return
-    }
-
-    var expect = chai.expect
-
-    var timer = setTimeout(
-      function() {
-        var message = "Got stuck in test \""+description+"\":\n"+runTest
-        if (setup) {
-          message += "\n... or or setup:\n"+setup
-        }
-        throw new Error(message)
-      },
-      1000
-    )
-
-    var runTest = test.bind(null, chai.expect)
-
-    var runAndDone = runTest.bind(null, function() {
-      clearTimeout(timer)
-      console.log("  ✓  "+description)
-    })
-
-    try {
+  var timer = setTimeout(
+    function() {
+      var message = "Got stuck in test \""+description+"\":\n"+runTest
       if (setup) {
-        setup(runAndDone)
-      } else {
-        runAndDone()
+        message += "\n... or or setup:\n"+setup
       }
-    } catch (e) {
-      console.log(" ⚡⚡⚡ "+description+" ⚡⚡⚡")
-      throw(e)
+      throw new Error(message)
+    },
+    1000
+  )
+
+  var runTest = test.bind(null, chai.expect)
+
+  var runAndDone = runTest.bind(null, function() {
+    clearTimeout(timer)
+    console.log("  ✓  "+description)
+  })
+
+  try {
+    if (setup) {
+      setup(runAndDone)
+    } else {
+      runAndDone()
     }
+  } catch (e) {
+    console.log(" ⚡⚡⚡ "+description+" ⚡⚡⚡")
+    throw(e)
   }
+}
 
-  test.only = function(description) {
-    only = description
-  }
+test.only = function(description) {
+  only = description
+}
 
-  return test
-})
+module.exports = test
